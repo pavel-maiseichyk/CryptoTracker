e3f2a91f1dafe5eadd4ab9ac0ee191c87c090141
pavel-maiseichyk <62174854+pavel-maiseichyk@users.noreply.github.com>
2024-10-17 22:13:05 +0200

    Create adaptive navigation with AdaptiveCoinListDetailPane

--- app/src/main/java/com/paulmais/cryptotracker/MainActivity.kt
+++ app/src/main/java/com/paulmais/cryptotracker/MainActivity.kt
@@ -1,28 +1,15 @@
 package com.paulmais.cryptotracker
 
 import android.os.Bundle
-import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.lifecycle.compose.collectAsStateWithLifecycle
-import com.paulmais.cryptotracker.core.presentation.util.ObserveAsEvents
-import com.paulmais.cryptotracker.core.presentation.util.toString
-import com.paulmais.cryptotracker.crypto.presentation.coin_detail.CoinDetailScreen
-import com.paulmais.cryptotracker.crypto.presentation.coin_list.CoinListEvent
-import com.paulmais.cryptotracker.crypto.presentation.coin_list.CoinListScreen
-import com.paulmais.cryptotracker.crypto.presentation.coin_list.CoinListViewModel
+import com.paulmais.cryptotracker.core.navigation.AdaptiveCoinListDetailPane
 import com.paulmais.cryptotracker.ui.theme.CryptoTrackerTheme
-import org.koin.androidx.compose.koinViewModel
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -31,35 +18,9 @@ class MainActivity : ComponentActivity() {
         setContent {
             CryptoTrackerTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    val viewModel = koinViewModel<CoinListViewModel>()
-                    val state by viewModel.state.collectAsStateWithLifecycle()
-                    val context = LocalContext.current
-                    ObserveAsEvents(events = viewModel.events) { event ->
-                        when (event) {
-                            is CoinListEvent.Error -> {
-                                Toast.makeText(
-                                    context,
-                                    event.error.toString(context),
-                                    Toast.LENGTH_LONG
-                                ).show()
-                            }
-                        }
-                    }
-                    when {
-                        state.selectedCoin != null -> {
-                            CoinDetailScreen(
-                                state = state,
-                                modifier = Modifier.padding(innerPadding)
-                            )
-                        }
-                        else -> {
-                            CoinListScreen(
-                                state = state,
-                                modifier = Modifier.padding(innerPadding),
-                                onAction = viewModel::onAction
-                            )
-                        }
-                    }
+                    AdaptiveCoinListDetailPane(
+                        modifier = Modifier.padding(innerPadding)
+                    )
                 }
             }
         }
